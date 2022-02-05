c2e6c30129b9d6ea83e19d3a5188ca6adf763b7a
Ceasar15 <ceasarkwadwo@gmail.com>
2022-02-05 18:39:22 +0000

    passport authentication

--- config/passport.js
+++ config/passport.js
@@ -3,21 +3,45 @@ const bcrypt = require('bcrypt');
 const User = require("../models/user")
 
 
-module.exports = function(passport){
+module.exports = function (passport) {
     passport.use(
         new LocalStrategy({
-            usernameField: 'email'}, (email, password, done) => {
-                // ?? match the User
-                User.findOne({
+            usernameField: 'email'
+        }, (email, password, done) => {
+            // ?? match the User
+            User.findOne({
                     email: email
                 })
                 .then((user) => {
-                    if (!user){
-                        return done(null, false, {message : "User is not found with that email"})
+                    if (!user) {
+                        return done(null, false, {
+                            message: "User is not found with that email"
+                        });
                     }
-                })
+                    //  match pass
+                    bcrypt.compare(password, user.pasword, (err, isMatch) => {
+                        if (err) throw err;
 
-            }
-        )
+                        if (isMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false, {
+                                message: "Incorrect Password"
+                            })
+                        }
+                    })
+                })
+                .catch((err) => {
+                    console.log("error", err);
+                })
+        })
     )
-}
+    passport.serializeUser(function(user, done){
+        done(null, user.id)
+    });
+    passport.deserializeUser(function(id, done){
+        User.findOneById(id, function(err, user){
+            done(err, user);
+        });
+    });
+};
